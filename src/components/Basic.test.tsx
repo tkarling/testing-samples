882e31b8d5e89542dd0b18504ff1fbd380fd6db9
tkarling <tkarling@live.com>
2019-08-03 11:06:19 -0700

    added @testing-library/react

--- src/components/Basic.test.tsx
+++ src/components/Basic.test.tsx
@@ -5,13 +5,20 @@ import ReactDOM from 'react-dom'
 
 import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
+import { render } from '@testing-library/react'
+
+it('renders without crashing plain', () => {
   const div = document.createElement('div')
   ReactDOM.render(<Basic />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
 
-it('renders without crashing', () => {
+it('renders without crashing enzyme', () => {
   const wrapper = shallow(<Basic />)
   expect(wrapper).toHaveText('Hello')
+})
+
+it('renders without crashing @testing-library/react ', () => {
+  const { getByText } = render(<Basic />)
+  expect(getByText('Hello')).toBeInTheDocument()
 })
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,11 @@
+// Following to setup react testing library
+// react-testing-library renders your components to document.body,
+// this will ensure they're removed after each test.
+import '@testing-library/react/cleanup-after-each'
+// this adds jest-dom's custom assertions
+import '@testing-library/jest-dom/extend-expect'
+
+// Following to setup enzyme
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import 'jest-enzyme'
