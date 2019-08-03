e30415e4e3b8c45f4afb2ff4433e29b60de66551
tkarling <tkarling@live.com>
2019-08-03 10:52:46 -0700

    added enzyme

--- src/components/Basic.test.tsx
+++ src/components/Basic.test.tsx
@@ -3,8 +3,15 @@ import Basic from './Basic'
 
 import ReactDOM from 'react-dom'
 
+import { shallow } from 'enzyme'
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<Basic />, div)
   ReactDOM.unmountComponentAtNode(div)
+})
+
+it('renders without crashing', () => {
+  const wrapper = shallow(<Basic />)
+  expect(wrapper).toHaveText('Hello')
 })
