c8859ae5899b9c54cb1d50e7f7dcda5cddbe2718
tkarling <tkarling@live.com>
2019-08-12 01:10:51 +0300

    timer base fake async api for todo list

--- src/api/service.test.ts
+++ src/api/service.test.ts
@@ -1,13 +1,49 @@
 import * as api from './service'
 
-const mockExpectedValue = 6
-jest.mock('../api/service', () => {
-  return { getValue: jest.fn(() => Promise.resolve(mockExpectedValue)) }
-})
+jest.useFakeTimers()
 
 describe('api', () => {
-  it('gets value from api', async () => {
-    const data = await api.getValue()
-    expect(data).toEqual(mockExpectedValue)
+  const callApi = async (fn: Function, payload: any = undefined) => {
+    const dataPromise = fn(payload)
+    jest.runAllTimers()
+    return await dataPromise
+  }
+  const expectItem = (
+    { title, completed }: Todo,
+    { title: expectedTitle, completed: expectedComplete }: Partial<Todo>
+  ) =>
+    title === expectedTitle &&
+    (completed !== undefined ? completed === expectedComplete : true)
+
+  it('gets value', async () => {
+    const data = await callApi(api.getValue)
+    expect(data).toEqual(api.FETCHED_VALUE)
+  })
+
+  it('gets todos', async () => {
+    const data = await callApi(api.getTodos)
+    expect(data).toEqual(api.FETCHED_TODOS)
+  })
+  it('adds todo', async () => {
+    const todoToAdd: Partial<Todo> = { title: 'added todo' }
+    const data = await callApi(api.addTodo, todoToAdd)
+    expect(data.length).toEqual(api.FETCHED_TODOS.length + 1)
+    expectItem(data[0], todoToAdd)
+  })
+
+  it('deletes todo', async () => {
+    const todoToDelete = api.FETCHED_TODOS[0]
+    const data = await callApi(api.deleteTodo, todoToDelete)
+    expect(data.length).toEqual(api.FETCHED_TODOS.length - 1)
+    expect(
+      data.find((todo: Todo) => todo.id === todoToDelete.id)
+    ).not.toBeDefined()
+  })
+
+  it('toggles todo', async () => {
+    const todoToToggle = api.FETCHED_TODOS[0]
+    const data = await callApi(api.toggleTodo, todoToToggle)
+    expect(data.length).toEqual(api.FETCHED_TODOS.length)
+    expectItem(data[0], { ...todoToToggle, completed: !todoToToggle.completed })
   })
 })
--- src/api/service.ts
+++ src/api/service.ts
@@ -1,6 +1,42 @@
 async function stall(stallTime = 3000) {
   await new Promise(resolve => setTimeout(resolve, stallTime))
 }
+const getId = () => Date.now() + ''
 
 export const FETCHED_VALUE = 5
 export const getValue = () => stall().then(() => FETCHED_VALUE)
+
+export const FETCHED_TODOS: Todo[] = [
+  {
+    id: '1',
+    title: 'moi',
+    completed: false
+  },
+  {
+    id: '2',
+    title: 'hei',
+    completed: false
+  }
+]
+
+let todos = FETCHED_TODOS
+export const getTodos = () => stall().then(() => todos)
+
+export const addTodo = ({ title, completed = false }: Partial<Todo>) =>
+  stall().then(() => {
+    return [{ id: getId, title, completed }, ...todos]
+  })
+
+export const deleteTodo = (todoToDelete: Todo) =>
+  stall().then(() => {
+    return todos.filter(todo => todo.id !== todoToDelete.id)
+  })
+
+export const toggleTodo = (todoToToggle: Todo) =>
+  stall().then(() => {
+    return todos.map(todo =>
+      todo.id !== todoToToggle.id
+        ? todo
+        : { ...todo, completed: !todo.completed }
+    )
+  })
